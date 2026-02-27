d6e1ce4d237775c729d78d3cd866bfcc125e00f9
dingyi_gu <gu002913@outlook.com>
2026-02-27 06:20:03 +0000

    first commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Panda-Compliance'
+copyright = '2026, Dingyi Gu'
+author = 'Dingyi Gu'
 
 release = '0.1'
 version = '0.1.0'
